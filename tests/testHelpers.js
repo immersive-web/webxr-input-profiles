ba7e17ded061e58706dbb3b235a8c769cd5d1ef0
Nell Waliczek <nhw@amazon.com>
2019-03-12 17:12:43 -0700

    Fixes path for case-sensitive volumes

--- tests/testHelpers.js
+++ tests/testHelpers.js
@@ -1,6 +1,6 @@
 const { join } = require('path');
 const Constants = require("../src/constants.js");
-const XRGamepad = require("../src/XRGamepad.js");
+const XRGamepad = require("../src/xrGamepad.js");
 
 const TestHelpers = {
   /**
