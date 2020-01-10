78e9d8337bc942b01f32d7db2786861d5f2f7f37
Nell Waliczek <nhw@amazon.com>
2020-01-10 08:11:19 -0800

    Refactor viewer to enable viewing controllers in vr (#128)
    * Refactor viewer to enable viewing controllers in vr
    
    This refactor separates the boilerplate three.js code from the part that
    is specific to using the webxr-input-profiles libraries.  It also unifies
    the profile selectors so that any local profile is added to the dropdown
    and will be used if the actual VR controller would be matched to it

---
+++ packages/viewer/src/assetError.js
@@ -0,0 +1,27 @@
+let errorsSectionElement;
+let errorsListElement;
+class AssetError extends Error {
+  constructor(...params) {
+    super(...params);
+    AssetError.log(this.message);
+  }
+
+  static initialize() {
+    errorsListElement = document.getElementById('errors');
+    errorsSectionElement = document.getElementById('errors');
+  }
+
+  static log(errorMessage) {
+    const itemElement = document.createElement('li');
+    itemElement.innerText = errorMessage;
+    errorsListElement.appendChild(itemElement);
+    errorsSectionElement.hidden = false;
+  }
+
+  static clearAll() {
+    errorsListElement.innerHTML = '';
+    errorsSectionElement.hidden = true;
+  }
+}
+
+export default AssetError;
