f62eeef11b6a1a81de634b52811653d78b68d45b
Brandon Jones <tojiro@gmail.com>
2020-01-22 11:48:29 -0800

    Misc preperations for publishing 1.0.0

--- packages/motion-controllers/rollup.config.js
+++ packages/motion-controllers/rollup.config.js
@@ -1,7 +1,14 @@
+const license = require('rollup-plugin-license');
+
 const DIST_FOLDER = 'dist';
 
 export default [
   {
+    plugins: [
+      license({
+        banner: '<%= pkg.name %> <%= pkg.version %> https://github.com/immersive-web/webxr-input-profiles'
+      })
+    ],
     input: ['src/index.js'],
     output: [
       {
